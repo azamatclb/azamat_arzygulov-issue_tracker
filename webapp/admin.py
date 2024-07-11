3ac366ca97394a5974ad4d22916d53a8cae7242c
azamat <azamatclb@gmail.com>
2024-07-11 15:51:11 +0600

    Создал модели задачи, статуса и типа.
    Создал миграцию и применил их.
    
    Зарегистрировал все модели в админ-панели.
    
    Создал админа и добавил через админ-панель типы и статусы задач, и задачи
    
    Создал фикстуру с данными из моих моделей + суперадмина, поместил её в папку fixtures

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
+from webapp.models import Task, Type, Status
+
+
 # Register your models here.
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'summary', "status", "type"]
+    list_display_links = ['id', 'summary', "status", "type"]
+    list_filter = ['status', 'type']
+    readonly_fields = ['added_date', 'updated_date']
+    search_fields = ['id', 'summary', "status", "type"]
+
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_links = ['id', 'name']
+
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_links = ['id', 'name']
+
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Type, TypeAdmin)
+admin.site.register(Status, StatusAdmin)
--- webapp/models.py
+++ webapp/models.py
@@ -1,12 +1,44 @@
 from django.db import models
 
+
 # Create your models here.
 
-class Task(models.Model):
-    pass
-
 class Type(models.Model):
-    pass
+    name = models.CharField(max_length=50, verbose_name="Тип задачи")
+
+    def __str__(self):
+        return f" {self.id} {self.name}"
+
+    class Meta:
+        db_table = "type"
+        verbose_name = "Тип"
+        verbose_name_plural = "Типы"
+
 
 class Status(models.Model):
-    pass
+    name = models.CharField(max_length=50, verbose_name="Статус задачи")
+
+    def __str__(self):
+        return f" {self.id} {self.name}"
+
+    class Meta:
+        db_table = "status"
+        verbose_name = "Статус"
+        verbose_name_plural = "Статусы"
+
+
+class Task(models.Model):
+    summary = models.CharField(max_length=50, verbose_name="Краткое описание")
+    description = models.TextField(max_length=3000, null=True, blank=True, verbose_name="Полное описание")
+    status = models.ForeignKey(Status, on_delete=models.CASCADE, verbose_name="Статус")
+    type = models.ForeignKey(Type, on_delete=models.CASCADE, verbose_name="Тип")
+    added_date = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    updated_date = models.DateTimeField(auto_now=True, verbose_name="Время обновления")
+
+    def __str__(self):
+        return f" {self.id} {self.summary} {self.status} {self.type}"
+
+    class Meta:
+        db_table = "Task"
+        verbose_name = "Задача"
+        verbose_name_plural = "Задачи"
